59eff93dcd0734c307aa9c9141cd4179e4d74393
Patrick Martin <patrickwaltermartin@gmail.com>
2024-01-03 16:25:19 -0400

    What started as a blog has now turned into just making my own toy version of Express

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 const http = require('http');
 const fs = require('fs');
 
-const PORT = 8000;
-const server = http.createServer((req, res) => {
-	console.log('A request has been made');
-	console.log(req);
+function app(){
+	const routes = new Map();
 
-	res.setHeader('Content-Type', 'text/html');
-	fs.readFile('./veiws/index.html', (err, data) => {
-		if (err){
-			console.log(err);
-			res.end();
-		} else{
-			res.write(data);
+	function server(port=8000, callBack) {
+		console.log(`Listening on port ${port}...`)
+		return http.createServer(requestHandler).listen(port, () => {
+			if (callBack){
+				console.log("hello sir");
+			}
+		});
+	}
+	
+	function get(path, func){
+		routes.set(`${path}/GET`, func)
+	}
+	function post(path, func){
+		routes.set(`${path}/POST`, func)
+	}
+
+	function requestHandler(req, res){
+		const url = req.url;
+		if(url !== '/favicon.ico'){
+			const method = req.method;
+			const urlRoute = `${url}/${method}`;
+			console.log(routes)
+			console.log(routes.get(urlRoute)(req, res));
+			res.write("Hello");
 			res.end();
 		}
-	});
+	}
 
+
+	return {get, post, routes, server}
+}
+
+
+test = app();
+test.get('/', (req, res) => {
+	//res.send('Hello World');
+	console.log('in the index');
 });
-
-
-server.listen(PORT, () => {
-	console.log(`listening on port ${PORT}...`);
-});
-
-
-
+test.get('/hello', '2');
+test.server();
+	//.listen(8000);
